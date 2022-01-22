61cdc653939c3db3556a73236279441866468ae4
Maksat Birimkulov <maxbirimkulov@mail.ru>
2022-01-22 14:37:30 +0600

    add keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID ,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAuvnBiPd9jYq8X3O0vXiJC04ajUkXVAsw",
+    authDomain: "brand-shop-a9926.firebaseapp.com",
+    projectId: "brand-shop-a9926",
+    storageBucket: "brand-shop-a9926.appspot.com",
+    messagingSenderId: "900494937362",
+    appId: "1:900494937362:web:ab2837e56c3af1964437cb",
+    measurementId: "G-2H8TQ15J8D"
 };
-
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
